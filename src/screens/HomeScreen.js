cc82b6c99b5696e89f6f551ee1b8140069304b0e
Saul-Ordonez <sauleordonez@gmail.com>
2020-04-16 16:25:41 -0700

    add image screen and route from home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button 
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title='Go to Components Demo'
       />
       <Button
         title='Go to List Demo'
-        onPress={() => props.navigation.navigate('List')}
+        onPress={() => navigation.navigate('List')}
+      />
+      <Button
+        title='Go to Image Demo'
+        onPress={() => navigation.navigate('Image')}
       />
     </View>
   );
