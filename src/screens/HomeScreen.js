bfaa12c7d9c8cc48fe35cef48c4ebabad82ba6c8
Saul-Ordonez <sauleordonez@gmail.com>
2020-04-16 16:16:51 -0700

    add navigation and refactor buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>Hi there!</Text>
       <Button 
-        onPress={() => console.log('Button pressed')}
+        onPress={() => props.navigation.navigate('Components')}
         title='Go to Components Demo'
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        title='Go to List Demo'
+        onPress={() => props.navigation.navigate('List')}
+      />
     </View>
   );
 };
