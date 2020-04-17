0dfeda1c04c9c41df0a081e2fe727d534d7fcbd2
Saul-Ordonez <sauleordonez@gmail.com>
2020-04-16 17:26:43 -0700

    add counter screen and connect to home with button

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet, button } from 'react-native';
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>Current Count:</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CounterScreen;
